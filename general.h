afbeb2b6d67704efe98ea7672aaa9ee62243bc46
yoelpro <yoelsusantopro@yahoo.com>
2018-11-16 15:57:44 +0700

    Add details on header comment and add updateGameState procedure

--- general.h
+++ general.h
@@ -1,8 +1,7 @@
-/* MODUL TABEL INTEGER */
-/* Berisi definisi dan semua primitif pemrosesan tabel integer */
-/* Penempatan elemen selalu rapat kiri */
-/* Versi I : dengan banyaknya elemen didefinisikan secara eksplisit, 
-   memori tabel statik */
+/*  File : general.h
+    berisi fungsi dan prosedur yang dibutuhkan program secara general
+
+*/
 
 #ifndef GENERAL_H
 #define GENERAL_H
@@ -13,6 +12,14 @@
 /* Definisi fungsi umum di program */
 void InitGame();
 /* Procedure untuk menginisialisasi game data untuk new game */
+
+void UpdateGameState();
+/*  Procedure yang menjalankan berbagai routine saat waktu bertambah 1 tick */
+/*  Diantaranya: 
+    1. Update customer yang sedang nungggu
+    2. Update customer yang sudah duduk dimeja
+    3. Nambah waktu
+    4. Add customer(probabilitas) */
 
 
 #endif
