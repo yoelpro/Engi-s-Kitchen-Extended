d0ae1014e0a2189e64ad1cd716a5559beaf061ed
yoelpro <yoelsusantopro@yahoo.com>
2018-11-16 15:57:05 +0700

    Erase ADT header comments in file

--- general.c
+++ general.c
@@ -1,10 +1,3 @@
-/* Nama: Stefanus Ardi Mulia */
-/* NIM: 13517119 */
-/* File : stackt.c */
-/* Tanggal: 17 Oktober 2018 */
-/* deklarasi stack yang diimplementasi dengan tabel kontigu dan ukuran sama */
-/* TOP adalah alamat elemen puncak */
-/* Implementasi dalam bahasa C dengan alokasi statik */
 #include "general.h"
 #include "data.h"
 
